b8a447c228d62b5293dd7ca269e3a1ab233ffbec
Luiz Claudio <74454646+lcsm212@users.noreply.github.com>
2021-11-16 15:36:04 -0300

    Projeto criado

---
+++ src/main/java/com/tubocerto/workshop/WorkshoptubocertoApplication.java
@@ -0,0 +1,13 @@
+package com.tubocerto.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshoptubocertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshoptubocertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tubocerto/workshop/WorkshoptubocertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubocerto.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshoptubocertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
