72daeb9f50f7765d776940e89f58604bc9ecc4f7
Luiz Claudio <74454646+lcsm212@users.noreply.github.com>
2021-11-16 17:44:45 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/tubocerto/workshop/resources/UserResource.java
+++ src/main/java/com/tubocerto/workshop/resources/UserResource.java
@@ -1,26 +1,26 @@
 package com.tubocerto.workshop.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tubocerto.workshop.domain.User;
+import com.tubocerto.workshop.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-	List<User> list = new ArrayList<>();
-		User Luiz = new User("1", "Luiz Claudio", "luiz.marzola@tubocerto.com", "975508268", "123456", "1");
-		User Mario = new User("2", "Mario", "marioa@tubocerto.com", "999999999", "123", "1");
-		list.addAll(Arrays.asList(Luiz, Mario));
+	List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
