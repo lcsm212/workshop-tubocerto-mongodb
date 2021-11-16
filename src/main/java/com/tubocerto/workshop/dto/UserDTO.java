c17f8f4fd46bded1c69fe2e1a2d2ba51b1fb797a
Luiz Claudio <74454646+lcsm212@users.noreply.github.com>
2021-11-16 18:28:02 -0300

    Usando padrão DTO para retornar usuários

---
+++ src/main/java/com/tubocerto/workshop/dto/UserDTO.java
@@ -0,0 +1,79 @@
+package com.tubocerto.workshop.dto;
+
+import java.io.Serializable;
+
+import com.tubocerto.workshop.domain.User;
+
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	private String filial;
+	
+	public UserDTO() {
+	}
+	
+	public UserDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+		phone = obj.getPhone();
+		password = obj.getPassword();
+		filial = obj.getFilial();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFilial() {
+		return filial;
+	}
+
+	public void setFilial(String filial) {
+		this.filial = filial;
+	}
+	
+	
+
+}
