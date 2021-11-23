62e67593e76edda7d87900d285c882e9870955f8
Luiz Claudio <74454646+lcsm212@users.noreply.github.com>
2021-11-23 14:19:08 -0300

    Inserção de usuário com POST

--- src/main/java/com/tubocerto/workshop/services/UserService.java
+++ src/main/java/com/tubocerto/workshop/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tubocerto.workshop.domain.User;
+import com.tubocerto.workshop.dto.UserDTO;
 import com.tubocerto.workshop.repository.UserRepository;
 import com.tubocerto.workshop.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,13 @@ public class UserService {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user;
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail(), objDTO.getPhone(), objDTO.getPassword(), objDTO.getFilial());
 	}
 }
